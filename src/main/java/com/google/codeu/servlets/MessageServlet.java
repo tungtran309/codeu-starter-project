1bf2290e2129558c07a46cb451212a9cce7d4cf9
tungtt <tungtt309@gmail.com>
2019-07-08 09:36:36 +0700

    remove image label scores

--- src/main/java/com/google/codeu/servlets/MessageServlet.java
+++ src/main/java/com/google/codeu/servlets/MessageServlet.java
@@ -84,9 +84,14 @@ public class MessageServlet extends HttpServlet {
     byte[] blobBytes = ImageAnalysisServlet.getBlobBytes(blobKey);
     List<EntityAnnotation> imageLabels = ImageAnalysisServlet.getImageLabels(blobBytes);
     StringBuilder prefix = new StringBuilder("\n");
+    int n_items = 3;
     prefix.append("<ul>");
     for(EntityAnnotation label : imageLabels){
-      prefix.append("<li>" + label.getDescription() + " " + label.getScore() + "\n" + "</li>");
+      prefix.append("<li>" + "#" + label.getDescription() + "\n" + "</li>");
+      n_items--;
+      if (n_items == 0) {
+        break;
+      }
     }
     prefix.append("</ul>");
 
